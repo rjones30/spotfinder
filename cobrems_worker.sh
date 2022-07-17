a9cf9a7586ec18e143b17d98f0a70206bedfe162
Richard T. Jones <richard.t.jones@uconn.edu>
2022-07-16 20:46:47 -0400

    * enhance the script for running the Celery worker [rtj]

--- cobrems_worker.sh
+++ cobrems_worker.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-cd ~/spotfinder_dev
+cd $(dirname $0)
 source setup.sh
-pkill -9 celery
-celery -A cobrems_worker worker
+if [ "$1" = "-h" -o "$1" = "--help" -o "$1" = "-?" ]; then
+    echo "Usage: cobrems_worker.sh [-f]"
+    exit 1
+elif [ "$1" = "-f" ]; then
+    pkill -9 -u $USER celery
+    sleep 1
+fi
+if ps -u $USER | grep -v grep | grep -q celery; then
+    echo "already running"
+else
+    celery -A cobrems_worker worker -l INFO >$(pwd)/cobrems_worker_$(hostname).log 2>&1 & 
+fi
