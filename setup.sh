b1d110c7a06c4fde3b35c6dc439063e986e3ac78
gluex user account <gluex@grendl.phys.uconn.edu>
2026-07-21 14:33:30 -0400

    * change environment variables to match the new deployment location and hostname [rtj]

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 export USER=$(whoami)
-export COBREMS_WORKER=/home/$USER/spotfinder
-export ROOTSYS=/home/osgusers/root-6.27.01/x86_64
+export COBREMS_WORKER=/nfs/direct/packages/gpfs/gpfs1/osgusers/spotfinder
+export ROOTSYS=//nfs/direct/packages/gpfs/gpfs1/osgusers/root_install
 export PATH=$PATH:/usr/local/bin:$ROOTSYS/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOTSYS/lib
 export PYTHONPATH=$PYTHONPATH:$ROOTSYS/lib
-export RABBITMQ_SERVER=cn410.storrs.hpc.uconn.edu
-export RABBITMQ_USER=spotfinder:FEA098B2D91AC7F723AE810
+export RABBITMQ_SERVER=nod30.phys.uconn.edu
+export RABBITMQ_USER=spotfinder:FEA04D199530F7F723AE810
 export RABBITMQ_VHOST=/spotfind
+export REDIS_SERVER=nod30.phys.uconn.edu
 
 # start firewall services
 #systemctl enable firewalld
