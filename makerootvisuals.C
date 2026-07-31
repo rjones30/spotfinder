468042399e073f90cb9c63c61232c126e4de3c90
Richard Jones <rjones30@gmail.com>
2026-07-31 08:53:32 -0400

    * update the list of build dependencies [rtj]

--- makerootvisuals.C
+++ makerootvisuals.C
@@ -4,10 +4,7 @@ void makerootvisuals() {
    std::cout << "Building the C++ rootvisuals_C.so and supporting libraries" << std::endl
              << "for the spotfinder tool. Make sure that LD_PRELOAD is not" << std::endl
              << "set in your environment, or you are in for a bad surprise!" << std::endl;
-   gSystem->AddIncludePath("-I/usr/include/python3.6m");
-   gSystem->AddIncludePath("-I./Analysis");
-   gSystem->AddIncludePath("-I.");
-   gSystem->Load("libboost_python3.so");
+   gSystem->AddIncludePath("-I/usr/include/python3.8");
    gSystem->CompileMacro("Map2D.cc", "kg");
    gSystem->CompileMacro("Couples.C", "kg");
    gSystem->AddIncludePath("-I/cvmfs/oasis.opensciencegrid.org/gluex/Diracxx/x86_64/include");
